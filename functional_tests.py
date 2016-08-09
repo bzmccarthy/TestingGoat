68f275e7d9064f79593d3dc987e771056b62d55b
bzmccarthy <bzmccarthy@gmail.com>
2016-08-09 11:52:02 -0400

    Added some additional code to save lists and display

--- functional_tests.py
+++ functional_tests.py
@@ -4,6 +4,7 @@ Python, Django, Selenium"""
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -40,27 +41,36 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page
         # lists "1: Buy peacock feathers" as an item in a to-do
-        # list
+        # list      
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows), 'New to-do item did not appear in table'
-            )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inviting her to add another
         # item. She enters "Use peacock feather to make a fly"
         # (Edith is very methodical)
-        self.fail('Finish the test!')
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+    
         # The page updates again, and now shows both items on
         # her list.
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make fly', [row.text for row in rows])
+
+        
 
         # Edith wonders whether the site will remember her list.
         # Then she sees that the site has generated a unique
         # URL for her -- there is some explanatory text to that
         # effect.
+
+        self.fail('Finish the test!')
 
         # She visits the URL = her to-do list is still there.
 
